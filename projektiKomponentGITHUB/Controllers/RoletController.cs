670e4afe559275cc1e565487bf84b852b5c1783f
HamzaLatifoviq <h.latifoviq220578@unite.edu.mk>
2025-05-03 21:51:34 +0200

    rolet

---
+++ projektiKomponentGITHUB/Controllers/RoletController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projektiKomponentGITHUB.Controllers
+{
+    public class RoletController : Controller
+    {
+        // GET: Rolet
+        public ActionResult Roli_Klient()
+        {
+            return View();
+        }
+        public ActionResult Roli_Admin()
+        {
+            return View();
+        }
+        public ActionResult Roli_HotelManager()
+        {
+            return View();
+        }
+        public ActionResult Roli_VeturManager()
+        {
+            return View();
+        }
+        public ActionResult Roli_Test()
+        {
+            return View();
+        }
+    }
+}
