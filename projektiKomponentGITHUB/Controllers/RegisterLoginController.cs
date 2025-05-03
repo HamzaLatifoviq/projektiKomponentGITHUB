4c29c1cc54138f6f6dc0f7892d73ec73ca30a807
HamzaLatifoviq <h.latifoviq220578@unite.edu.mk>
2025-05-03 18:29:01 +0200

    Register login view ene layout cshtml

---
+++ projektiKomponentGITHUB/Controllers/RegisterLoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projektiKomponentGITHUB.Controllers
+{
+    public class RegisterLoginController : Controller
+    {
+        // GET: RegisterLogin
+        public ActionResult RegisterView()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
