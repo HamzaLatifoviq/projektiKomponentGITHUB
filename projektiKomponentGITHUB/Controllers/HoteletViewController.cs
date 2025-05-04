96ff9714f4181f3ee9c2f1896f850cb02d501566
BesnikMurseli <b.murseli220082@unite.edu.mk>
2025-05-04 22:29:24 +0200

    hotelet view

---
+++ projektiKomponentGITHUB/Controllers/HoteletViewController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projektiKomponentGITHUB.Controllers
+{
+    public class HoteletViewController : Controller
+    {
+        // GET: HoteletView
+        public ActionResult Hotel1()
+        {
+            return View();
+        }
+        public ActionResult Hotel2()
+        {
+            return View();
+        }
+        public ActionResult Hotel3()
+        {
+            return View();
+        }
+        public ActionResult Hotel4()
+        {
+            return View();
+        }
+        public ActionResult Hotel5()
+        {
+            return View();
+        }
+        public ActionResult Hotel6()
+        {
+            return View();
+        }
+    }
+}
