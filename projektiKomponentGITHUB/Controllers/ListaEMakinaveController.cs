a2d07e06d0494b7d3421d6af41441190e66d62c7
ArtinKadrija <artin.kadrija001@gmail.com>
2025-05-04 22:14:50 +0200

    viewsat e makinave

---
+++ projektiKomponentGITHUB/Controllers/ListaEMakinaveController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projektiKomponentGITHUB.Controllers
+{
+    public class ListaEMakinaveController : Controller
+    {
+        // GET: ListaEMakinave
+        public ActionResult View1()
+        {
+            return View();
+        }
+        public ActionResult View2()
+        {
+            return View();
+        }
+        public ActionResult View3()
+        {
+            return View();
+        }
+        public ActionResult View4()
+        {
+            return View();
+        }
+        public ActionResult View5()
+        {
+            return View();
+        }
+        public ActionResult View6()
+        {
+            return View();
+        }
+        public ActionResult View7()
+        {
+            return View();
+        }
+        public ActionResult View8()
+        {
+            return View();
+        }
+        public ActionResult View9()
+        {
+            return View();
+        }
+    }
+}
