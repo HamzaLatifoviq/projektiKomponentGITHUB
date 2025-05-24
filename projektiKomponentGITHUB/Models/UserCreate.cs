3de00d1bc897341e9fdfc71a3e0dada261d64ad4
HamzaLatifoviq <h.latifoviq220578@unite.edu.mk>
2025-05-24 17:09:23 +0200

    usercreate

--- projektiKomponentGITHUB/Models/UserCreate.cs
+++ projektiKomponentGITHUB/Models/UserCreate.cs
@@ -7,5 +7,11 @@ namespace projektiKomponentGITHUB.Models
 {
     public class UserCreate
     {
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
