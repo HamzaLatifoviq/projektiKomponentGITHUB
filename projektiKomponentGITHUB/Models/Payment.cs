c5df8a5d1fce7ccad203c6ff19e27a8fbab6c620
HamzaLatifoviq <h.latifoviq220578@unite.edu.mk>
2025-05-24 17:22:20 +0200

    Payment kontrolleri

--- projektiKomponentGITHUB/Models/Payment.cs
+++ projektiKomponentGITHUB/Models/Payment.cs
@@ -1,24 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿using System.ComponentModel.DataAnnotations;
+using System;
 namespace projektiKomponentGITHUB.Models
 {
     public class Payment
     {
-       
-            public int Id { get; set; }
-            public string Emri { get; set; }
-            public string Mbiemri { get; set; }
-            public string Emaili { get; set; }
-            public string Adresa { get; set; }
-            public string NumriTelefonit { get; set; }
-            public string NumriKartes { get; set; }
-            public DateTime DataSkadimit { get; set; }
-            public string CVV { get; set; }
-            public string MenyraPageses { get; set; }
-            public decimal Shuma { get; set; }
-            public string PaymentStatus { get; set; } = "Pending";
-        }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Emri është i nevojshëm")]
+        public string Emri { get; set; }
+
+        [Required(ErrorMessage = "Mbiemri është i nevojshëm")]
+        public string Mbiemri { get; set; }
+
+        [Required, EmailAddress(ErrorMessage = "Emaili është i pavlefshëm")]
+        public string Emaili { get; set; }
+
+        [Required]
+        public string Adresa { get; set; }
+
+        [Required]
+        public string NumriTelefonit { get; set; }
+
+        [Required]
+        public string NumriKartes { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DataSkadimit { get; set; }
+
+        [Required]
+        public string CVV { get; set; }
+
+        [Required]
+        public string MenyraPageses { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Shuma duhet të jetë më e madhe se zero")]
+        public decimal Shuma { get; set; }
+
+        public DateTime DataPageses { get; set; }
+
+        public string PaymentStatus { get; set; }
     }
+}
