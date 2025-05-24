b18b8ab13cfd3bd3cd2b8f1d68e61f3ba702bbd0
HamzaLatifoviq <h.latifoviq220578@unite.edu.mk>
2025-05-24 17:08:08 +0200

    UserViewModel

--- projektiKomponentGITHUB/Models/UserViewModel.cs
+++ projektiKomponentGITHUB/Models/UserViewModel.cs
@@ -7,5 +7,15 @@ namespace projektiKomponentGITHUB.Models
 {
     public class UserViewModel
     {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastLogin { get; set; }
     }
+
 }
