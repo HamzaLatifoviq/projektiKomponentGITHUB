a1eced9930df1a45b564f68f4613f0b1d6deb05b
HamzaLatifoviq <h.latifoviq220578@unite.edu.mk>
2025-05-24 17:08:44 +0200

    user model

--- projektiKomponentGITHUB/Models/User.cs
+++ projektiKomponentGITHUB/Models/User.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace projektiKomponentGITHUB.Models
 {
     public class User
     {
+        public int UserId { get; set; }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } = "Client";  // default role when registering
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? LastLogin { get; set; }
     }
 }
