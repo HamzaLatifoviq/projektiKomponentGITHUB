14d7d655e83bbf4a6959f26ae8ea5e74e84c37b0
HamzaLatifoviq <h.latifoviq220578@unite.edu.mk>
2025-05-25 15:16:56 +0200

    ListaEMakinaveController.cs

--- projektiKomponentGITHUB/Controllers/ListaEMakinaveController.cs
+++ projektiKomponentGITHUB/Controllers/ListaEMakinaveController.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using projektiKomponentGITHUB.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
 
 namespace projektiKomponentGITHUB.Controllers
@@ -9,41 +10,137 @@ namespace projektiKomponentGITHUB.Controllers
     public class ListaEMakinaveController : Controller
     {
         // GET: ListaEMakinave
-        public ActionResult View1()
+        public ActionResult View1() => View();
+        public ActionResult View2() => View();
+        public ActionResult View3() => View();
+        public ActionResult View4() => View();
+        public ActionResult View5() => View();
+        public ActionResult View6() => View();
+        public ActionResult View7() => View();
+        public ActionResult View8() => View();
+        public ActionResult View9() => View();
+
+        public ActionResult Details(int? id)
         {
-            return View();
+            using (var db = new MyDbContext())
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                var vetura = db.Veturat.Find(id);
+                if (vetura == null)
+                {
+                    return HttpNotFound();
+                }
+
+                var model = new BookingViewModel
+                {
+                    VeturaId = vetura.Id,
+                    Emri = vetura.Emri,
+                    Kategoria = vetura.Kategoria,
+                    Qyteti = vetura.Qyteti,
+                    Distanca = vetura.Distanca,
+                    Transmetimi = vetura.Transmetimi,
+                    LlojiKarburantit = vetura.LlojiKarburantit,
+                    Pershkrimi = vetura.Pershkrimi,
+                    FotoPath = vetura.FotoPath,
+                    Price = vetura.Price
+                };
+
+                return View(model);
+            }
         }
-        public ActionResult View2()
+
+        // POST: CreateBooking
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateBooking(BookingViewModel model)
         {
-            return View();
+            using (var db = new MyDbContext())
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View("Details", model);
+                }
+
+                if (!model.PickupDate.HasValue)
+                {
+                    ModelState.AddModelError("PickupDate", "Ju lutem zgjidhni datën e marrjes së makinës.");
+                    return View("Details", model);
+                }
+
+                var vehicle = db.Veturat.Find(model.VeturaId);
+                if (vehicle == null)
+                {
+                    ModelState.AddModelError("", "Makina nuk u gjet.");
+                    return View("Details", model);
+                }
+
+                int rentalDays = 1;
+                if (model.PickupDate.HasValue && model.DropoffDate.HasValue)
+                {
+                    rentalDays = (model.DropoffDate.Value - model.PickupDate.Value).Days;
+                    if (rentalDays <= 0) rentalDays = 1;
+                }
+
+                decimal vehicleTotalPrice = vehicle.Price * rentalDays;
+
+                decimal addonsTotalPrice = 0m;
+                if (model.GPS) addonsTotalPrice += 5m;
+                if (model.BabySeat) addonsTotalPrice += 10m;
+                if (model.ExtraInsurance) addonsTotalPrice += 20m;
+                if (model.AdditionalDriver) addonsTotalPrice += 50m;
+
+                decimal totalPriceAtBooking = vehicleTotalPrice + addonsTotalPrice;
+
+                string addOns = string.Join(",", new[] {
+                    model.GPS ? "GPS" : null,
+                    model.BabySeat ? "BabySeat" : null,
+                    model.ExtraInsurance ? "ExtraInsurance" : null,
+                    model.AdditionalDriver ? "AdditionalDriver" : null
+                }.Where(x => x != null));
+
+                int? currentUserId = null;
+                if (Session["UserId"] != null)
+                {
+                    currentUserId = (int)Session["UserId"];
+                }
+
+                var booking = new VeturBooking
+                {
+                    VeturaID = model.VeturaId,
+                    DropOffLocation = model.DropOffLocation,
+                    AddOns = addOns,
+                    BookingDate = DateTime.Now,
+                    PickupDate = model.PickupDate,
+                    DropoffDate = model.DropoffDate,
+                    GPS = model.GPS,
+                    BabySeat = model.BabySeat,
+                    ExtraInsurance = model.ExtraInsurance,
+                    AdditionalDriver = model.AdditionalDriver,
+                    UserID = currentUserId,
+                    PriceAtBooking = totalPriceAtBooking
+                };
+
+                db.Bookings.Add(booking);
+                db.SaveChanges();
+
+                TempData["SuccessMessage"] = "Rezervimi u krye me sukses!";
+
+                // Redirect to the payment page with the total price
+                return RedirectToAction("PagesatView", "PagesaTransaksionet", new { shuma = totalPriceAtBooking });
+            }
         }
-        public ActionResult View3()
+
+        protected override void Dispose(bool disposing)
         {
-            return View();
-        }
-        public ActionResult View4()
-        {
-            return View();
-        }
-        public ActionResult View5()
-        {
-            return View();
-        }
-        public ActionResult View6()
-        {
-            return View();
-        }
-        public ActionResult View7()
-        {
-            return View();
-        }
-        public ActionResult View8()
-        {
-            return View();
-        }
-        public ActionResult View9()
-        {
-            return View();
+            if (disposing)
+            {
+                // nothing to dispose manually
+            }
+            base.Dispose(disposing);
         }
     }
 }
