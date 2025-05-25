edca0689b794eb405b16f96497df5865b93bcdfb
HamzaLatifoviq <h.latifoviq220578@unite.edu.mk>
2025-05-25 14:59:47 +0200

    modelet

---
+++ projektiKomponentGITHUB/Controllers/VeturatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projektiKomponentGITHUB.Controllers
+{
+    public class VeturatController : Controller
+    {
+        // GET: Veturat
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
